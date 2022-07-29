ba153982e335f22d4cf6994ac4363a204aa74ba9
FreddyCro <freddy10100@hotmail.com>
2022-07-29 12:46:20 +0800

    fix: style

--- client/src/components/fk-footer.vue
+++ client/src/components/fk-footer.vue
@@ -79,7 +79,7 @@ export default {
       position: absolute;
       top: 50%;
       right: 0;
-      height: 36px;
+      height: 24px;
       border-right: #000 1px solid;
       transform: translate(0, -50%);
     }
@@ -97,6 +97,10 @@ export default {
     @include rwd-min(sm) {
       height: 40px;
       padding: 0 $spacing-6;
+
+      &::before {
+        height: 36px;
+      }
     }
 
     @include rwd-min(md) {
